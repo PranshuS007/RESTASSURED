100e5e7b6e928d3b5c0f06fc807752ac3fcc50c2
Pranshu Sharma <pranshusharma712@gmail.com>
2022-06-02 13:06:39 +0530

    first

---
+++ src/main/java/apitesting/sampleproject/App.java
@@ -0,0 +1,13 @@
+package apitesting.sampleproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
